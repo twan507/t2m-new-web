00cdf0edb6961371c3496f59038ca9c013bb3027
“twan” <tuanbuianh507@gmail.com>
2025-03-12 21:02:50 +0700

    last final

--- t2m-backend/src/app.module.ts
+++ t2m-backend/src/app.module.ts
@@ -47,11 +47,11 @@ require('dotenv').config()
 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '14.225.192.30',
-      port: 3306,
-      username: 'twan',
-      password: 'chodom',
-      database: 't2m',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT) || 3306,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       extra: {
         connectionLimit: 10,
       },
